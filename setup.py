588afd66ae184eb040bf99b6b23f4da34f1d00fd
Mikael Koli <koli.mikael@gmail.com>
2020-04-20 21:11:30 +0300

    setup: exanded

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="JuBox",
     version="0.1",
-    packages=find_packages(),
-    long_description=open('README.md').read(),
     author="Mikael Koli",
-    author_email="koli.mikael@gmail.com",
+    author_email="koli.mikael@op.fi",
+    packages=find_packages(),
+    description="Object oriented interface to Jupyter Notebook",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+     classifiers=[
+         "Programming Language :: Python :: 3",
+     ],
+     include_package_data=True # for MANIFEST.in
 )
