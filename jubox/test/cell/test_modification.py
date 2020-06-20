c81e9b3fc3553cb53e63cc1b11a2c6907f6bde30
Mikael Koli <koli.mikael@gmail.com>
2020-06-20 08:21:32 +0300

    test: added new test cases to cell modification testing, cleaned the unneeded imports and added minimal docs about the test file

--- jubox/test/cell/test_modification.py
+++ jubox/test/cell/test_modification.py
@@ -1,15 +1,14 @@
 
+"""
+This test file contains tests for manipulating the
+source of each cell class (test_classes), 
+ie. the code in CodeCell, the markdown in MarkDownCell etc.
+"""
+
+
 import pytest
 
-from jubox import CodeCell, MarkdownCell, RawCell, JupyterCell
-from nbformat.notebooknode import NotebookNode
-
-from nbformat.v4 import new_code_cell, new_markdown_cell, new_raw_cell
-
-test_classes = [CodeCell, MarkdownCell, RawCell]
-
-def get_test_id(tpl):
-    return tpl.__name__
+from utils import test_classes, get_test_id
 
 @pytest.mark.parametrize("cls,", test_classes, ids=get_test_id)
 def test_append(cls, *args):
@@ -28,3 +27,21 @@ def test_insert(cls):
     cell = cls("foo string")
     cell.insert(3, " bar")
     assert cell._node["source"] == "foo bar string"
+
+@pytest.mark.parametrize("cls,", test_classes, ids=get_test_id)
+def test_insert_another_cell(cls):
+    cell = cls("foo string")
+    cell.insert(3, cls(" bar"))
+    assert cell._node["source"] == "foo bar string"
+
+@pytest.mark.parametrize("cls,", test_classes, ids=get_test_id)
+def test_overwrite(cls):
+    cell = cls("foo string")
+    cell.overwrite("now its bar string")
+    assert cell._node["source"] == "now its bar string"
+
+@pytest.mark.parametrize("cls,", test_classes, ids=get_test_id)
+def test_overwrite_another_cell(cls):
+    cell = cls("foo string")
+    cell.overwrite(cls("now its bar string"))
+    assert cell._node["source"] == "now its bar string"
